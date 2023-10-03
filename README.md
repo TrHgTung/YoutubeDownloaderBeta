ba02ffc4121d8850562ee2346daad96a2f88253a
Agile101hcmue <89836403+TrHgTung@users.noreply.github.com>
2023-10-03 22:36:20 +0700

    init commit

---
+++ README.md
@@ -0,0 +1,52 @@
+<h1 align='center'>Youtube Video Downloader</h1>
+<p align='center'>YouTube Video Downloader is a simple Python Web application built with Django</p>
+<p>youtube-dll module is used: <a href="https://github.com/ytdl-org/youtube-dl">youtube-dll</a></p>
+
+## Source Code (original)
+
+[![Github@alii76tt](https://github.com/alii76tt/youtube-video-downloader)](https://github.com/alii76tt/youtube-video-downloader)
+
+## Live Demo
+
+[![Heroku](https://heroku-badge.herokuapp.com/?app=heroku-badge)](https://youtube-video-download-yt.herokuapp.com/)
+
+<p align="center">
+    <a href="https://alii76ttytclone.pythonanywhere.com/">
+        <img src="./images/1.gif"
+            alt="Update Github Stats" title="django-yt-clone">
+    </a>
+</p>
+
+## Requirements
+
+This App Uses
+
+```
+asgiref==3.5.1
+Django==3.2.3
+pytz==2022.1
+sqlparse==0.4.2
+typing-extensions==4.2.0
+youtube-dl==2021.12.17
+```
+
+# Installation
+
+```sh
+$ git clone https://github.com/alii76tt/youtube-video-downloader
+$ cd youtube-video-downloader
+$ python -m venv venv
+if you are using windows:
+$.\venv\Scripts\activate
+if you are using linux
+$source venv/bin/activate
+$ pip install -r requirements.txt
+$ python manage.py runserver
+```
+
+## Screenshots
+
+<p align="center">
+    <img src="./images/1.png">
+    <img src="./images/2.png">
+</p
---
+++ youtube/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render
+from .forms import DownloadForm
+# package
+import youtube_dl as yt
+# check video url
+import re
+
+
+def index(request):
+    form = DownloadForm(request.POST or None)
+    if form.is_valid():
+        url = form.cleaned_data.get("url")
+        # check video url
+        regex = r'^(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+'
+        if not re.match(regex, url):
+            context = {
+                'error': "Please enter correct url!",
+                'form': form
+            }
+            return render(request, 'home.html', context)
+
+        yt_opts = {}
+
+        with yt.YoutubeDL(yt_opts) as ydl:
+            try:
+                meta = ydl.extract_info(
+                    url, download=False)
+                video = True
+            except:
+                context = {
+                    'error': "Video unavailable. Please enter correct url!",
+                    'form': form
+                }
+                return render(request, 'home.html', context)
+
+        video_audio_streams = []
+        audioStreams = []
+        for item in meta['formats']:
+            file_size = item['filesize']
+            itemFormat = item['format_note']
+
+            resolution = 'Audio'
+            if item['height'] is not None:
+                resolution = f"{item['height']}x{item['width']}"
+
+            if 'Audio' in resolution:
+                audioStreams.append({
+                    'resolution': resolution,
+                    'extension': item['ext'],
+                    'file_size': file_size,
+                    'itemFormat': itemFormat,
+                    'url': item['url'],
+                })
+            else:
+                video_audio_streams.append({
+                    'resolution': resolution,
+                    'extension': item['ext'],
+                    'file_size': file_size,
+                    'itemFormat': itemFormat,
+                    'url': item['url']
+                })
+        video_audio_streams = video_audio_streams[::-1]
+
+        context = {
+            'form': form,
+            'isVideo': video,
+            'url': url,
+            'title': meta['title'],
+            'streams': video_audio_streams,
+            'audioStreams': audioStreams,
+            'channel': meta['channel'],
+            'image': meta['thumbnails'][3]['url'],
+            'duration': round(int(meta['duration'])/60, 2),
+            'views': f'{int(meta["view_count"]):,}'
+        }
+        return render(request, 'home.html', context)
+    return render(request, 'home.html', {'form': form})
+
+def about(request):
+    return render(request,"about.html")
